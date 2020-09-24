fac0013a762ff480ce1adc07128074f8f643be0f
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2020-09-24 23:41:36 +0200

    csv test cases

--- src/store/modules/csv.js
+++ src/store/modules/csv.js
@@ -3,12 +3,38 @@ import stringify from 'csv-stringify/lib/sync';
 import * as samples from '@/samples';
 
 const LABEL = 'label';
-const OPTIONS = {
+export const OPTIONS = {
     delimiter: ',',
     columns: false,
     skip_empty_lines: true,
     cast: true,
-    cast_date: true
+    cast_date: true,
+    trim: true
+};
+
+/** Parses input csv to json object  */
+export const parseInput = function(
+    input,
+    options = OPTIONS,
+    withHeaders = true,
+    result = { headers: [], data: [] }
+) {
+    if (input) {
+        var records = parse(input, options);
+        if (withHeaders) {
+            result.headers = records.slice(0, 1)[0];
+            result.data = records.slice(1);
+        } else {
+            result.headers = [...Array(records[0].length).keys()].map(x =>
+                String.fromCharCode('a'.charCodeAt(0) + x)
+            );
+            result.data = records;
+        }
+        if (result.headers.indexOf(LABEL) < 0) {
+            result.headers.push(LABEL);
+        }
+    }
+    return result;
 };
 
 export default {
@@ -18,40 +44,45 @@ export default {
         data: [],
         headers: [],
         output: '',
-        example: samples.EXAMPLE
+        example: samples.EXAMPLE,
+        inputWithHeaders: true
     }),
     mutations: {
-        load(state, input) {
+        input(state, input) {
             state.input = input;
         },
-        parse(state) {
-            if (state.input) {
-                var records = parse(state.input, OPTIONS);
-                state.data = records.slice(1);
-                state.headers = records.slice(0, 1)[0];
-                if (state.headers.indexOf(LABEL) < 0) {
-                    state.headers.push(LABEL);
-                }
-            }
+        data(state, input) {
+            state.data = input;
         },
-        write(state, payload) {
-            if (payload) state.data = payload;
-            if (state.data) {
-                state.output = stringify(state.data, {
-                    header: true,
-                    columns: state.headers
-                });
-            }
+        headers(state, input) {
+            state.headers = input;
+        },
+        output(state, input) {
+            state.output = stringify(input, {
+                header: true,
+                columns: state.headers
+            });
         }
     },
     actions: {
-        read: function({ commit }, payload) {
-            commit('load', payload);
-            commit('parse');
-            commit('write');
+        read: function({ commit, state }, payload) {
+            const records = parseInput(
+                payload,
+                OPTIONS,
+                state.inputWithHeaders
+            );
+            commit('input', payload);
+            commit('data', records.data);
+            commit('headers', records.headers);
+            if (records.data) {
+                commit('output', records.data);
+            }
         },
         write: function({ commit }, payload) {
-            commit('write', payload);
+            commit('data', payload);
+            if (payload) {
+                commit('output', payload);
+            }
         }
     },
     getters: {}
