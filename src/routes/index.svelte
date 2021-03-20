487c5cf6ea648bdfcda24e76aceefef2c26e4088
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2021-03-20 15:02:35 +0100

    removed the template code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,11 @@
 <script lang="ts">
     import { browser } from "$app/env";
-    import Counter from "$lib/Counter.svelte";
     import App from "$lib/App.svelte";
 </script>
 
 <main>
-    <h1>Hello world!</h1>
 
     {#if browser}
         <App />
     {/if}
-    <Counter />
-
-    <p>
-        Visit <a class="text-blue-600 underline" href="https://svelte.dev"
-            >svelte.dev</a
-        > to learn how to build Svelte apps.
-    </p>
 </main>
-
-<style lang="scss">
-    :root {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    }
-
-    main {
-        @apply text-center;
-        @apply p-4;
-        @apply mx-auto;
-    }
-
-    h1 {
-        @apply text-red-600;
-        @apply uppercase;
-        @apply text-6xl;
-        @apply font-thin;
-        @apply leading-tight;
-        @apply my-16 mx-auto;
-        @apply max-w-xs;
-    }
-
-    p {
-        @apply max-w-xs;
-        @apply my-8 mx-auto;
-        @apply leading-snug;
-    }
-
-    @screen sm {
-        h1 {
-            @apply max-w-none;
-        }
-
-        p {
-            @apply max-w-none;
-        }
-    }
-</style>
