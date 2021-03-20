e43c84478d26c838443f0f764e06e2ad7b3d804a
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2021-03-20 17:24:24 +0100

    replaced the adapter with source and aligned with recent version

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,31 +1,43 @@
 const sveltePreprocess = require('svelte-preprocess');
 const pkg = require('./package.json');
-
+let { pages = 'build', assets = 'build' } = {}
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		sveltePreprocess({
-			defaults: {
-				style: "postcss",
-			},
-			postcss: true
-		}),
-	],
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: '@sveltejs/adapter-static',
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        sveltePreprocess({
+            defaults: {
+                style: "postcss",
+            },
+            postcss: true
+        }),
+    ],
+    kit: {
+        // By default, `npm run build` will create a standard Node app.
+        // You can create optimized builds for different platforms by
+        // specifying a different adapter
+        adapter: {
+            name: '@sveltejs/adapter-static',
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+            async adapt(builder) {
+                builder.copy_static_files(assets);
+                builder.copy_client_files(assets);
 
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	}
+                await builder.prerender({
+                    force: true,
+                    dest: pages
+                });
+            }
+        },
+
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+
+        vite: {
+            ssr: {
+                noExternal: Object.keys(pkg.dependencies || {})
+            }
+        }
+    }
 };
