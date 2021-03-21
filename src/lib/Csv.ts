ef1e3fe18207f800d3e2f51b85d1a20222bce67e
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2021-03-21 13:27:30 +0100

    csv paring issue with leading whitespace
    seems to be both values and headers that have the leading whitespace issue

--- src/lib/Csv.ts
+++ src/lib/Csv.ts
@@ -5,12 +5,10 @@ const { parse, unparse } = paraparse // TODO: figure out why destructuring fails
 import { writable, derived } from "svelte/store";
 import { sample } from "./Sample";
 
-// csv library does cannot handle leading whitespace when headers are quoted like in this example https://people.sc.fsu.edu/~jburkardt/data/csv/trees.csv
+// csv library does cannot handle leading whitespace when values or headers are quoted like in this example https://people.sc.fsu.edu/~jburkardt/data/csv/trees.csv
 const trim = function (input: string): string {
     try {
-        let [head, ...tail] = input.split('\n');
-        head = head.replace(/",\s*"/gi, '","');
-        return [head, ...tail].join('\n')
+        return input.replace(/",\s*"/gi, '","');
     } catch { return input }
 }
 
@@ -37,7 +35,7 @@ export const parsedData = derived(inputCsv, (x) => {
         transformHeader: (value) => value.trim(),
         transform: (value) => value.trim()
     })
-    
+
     const dataset = result.data.map(_.values);
     const headers = result.meta.fields;
     const transposed = _.zip.apply(_, dataset);
